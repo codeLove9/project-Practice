58a21c6b9a2980c146c55a43cd98ab2c05c9eafc
“Luck” <1071626267@qq.com>
2023-03-03 21:19:09 +0800

    手机调试测试

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    // 原为: hotst: 'localhost'
+    host: '192.168.1.4',
+    port: 8080
+  }
 })
